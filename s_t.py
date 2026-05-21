de98e0db1c0016bd7994d451e2c669358ad92e8c
juanitanassarlondono-cyber <juanitanassarlondono@gmail.com>
2026-05-21 08:39:15 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -19,7 +19,7 @@ st.title("TRADUCTOR.")
 st.subheader("Escucho lo que quieres traducir.")
 
 
-image = Image.open('OIG7.jpg')
+image = Image.open('traductor.jpeg')
 
 st.image(image,width=300)
 with st.sidebar:
@@ -181,15 +181,3 @@ if result:
                     print("Deleted ", f)
 
     remove_files(7)
-           
-
-
-        
-    
-
-
-
-        
-    
-
-
